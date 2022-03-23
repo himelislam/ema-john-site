1b77ca239d3403cd7eeadf9ed24cebabf389436a
himelislam <himel7100@gmail.com>
2022-03-23 12:49:03 +0600

    shop page updated

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {name, img, seller, price, ratings} = props.product
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button onClick={() => props.handle(props.product)} className='btn-cart'>
+                <p className='btn-text'>Add To Cart</p>
+                <p>Cartpic</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
